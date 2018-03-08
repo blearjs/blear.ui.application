3dd4a7ce43781d9a74798af20e668e5740ac4881
#云淡然 <cloudcome@163.com>
2018-03-08 14:16:15 +0800

    + 重构

--- src/index.js
+++ src/index.js
@@ -9,3 +9,125 @@
 
 'use strict';
 
+var UI = require('blear.ui');
+var attribute = require('blear.core.attribute');
+var modification = require('blear.core.modification');
+var selector = require('blear.core.selector');
+var object = require('blear.utils.object');
+
+var View = require('./view');
+
+var namespace = 'blearui-application';
+var defaults = {
+    /**
+     * 根元素
+     */
+    el: null,
+
+    /**
+     * 平台，可选：mobile/desktop
+     */
+    platform: 'mobile',
+
+    /**
+     * 显示动画，可以根据参数来实现过场动画
+     * @param el
+     * @param viewOptions
+     * @param done
+     */
+    showAnimation: function (el, viewOptions, done) {
+        done();
+    },
+
+    /**
+     * 隐藏动画，可以根据参数来实现过场动画
+     * @param el
+     * @param viewOptions
+     * @param done
+     */
+    hideAnimation: function (el, viewOptions, done) {
+        done();
+    }
+};
+var Application = UI.extend({
+    className: 'Application',
+    conrtuctor: function (router, options) {
+        var the = this;
+
+        Application.parent(the);
+        the[_router] = router;
+        the[_options] = object.assign({}, defaults, options);
+        the[_initNode]();
+        the[_viewMap] = {};
+    },
+
+    /**
+     * 销毁
+     */
+    destroy: function () {
+        var the = this;
+
+        the[_router]
+            = the[_options]
+            = the[_viewMap]
+            = null;
+        Application.invoke('destroy', the);
+    }
+});
+var prop = Application.prototype;
+var sole = Application.sole;
+var _router = sole();
+var _options = sole();
+var _viewsEl = sole();
+var _initNode = sole();
+var _initEvent = sole();
+var _getView = sole();
+var _viewMap = sole();
+
+prop[_initNode] = function () {
+    var the = this;
+    var options = the[_options];
+
+    the[_viewsEl] = selector.query(options.el)[0];
+    attribute.addClass(the[_viewsEl], namespace + '-views');
+};
+
+prop[_initEvent] = function () {
+    var the = this;
+
+    the[_router].on('afterChange', function (route) {
+        var ctrl = route.controller;
+        var nextView = the[_getView](route);
+        var prevView = the[_getView](route.prev);
+
+        // 路由方向不变
+        if (route.direction === 'replace') {
+            nextView.replace(route, ctrl);
+        }
+        // 路由方向变化
+        else {
+            if (prevView) {
+                prevView.leave(route, ctrl, function () {
+                    nextView.enter(route, ctrl);
+                });
+            } else {
+                nextView.enter(route, ctrl);
+            }
+        }
+    });
+};
+
+prop[_getView] = function (route) {
+    var the = this;
+
+    if (!route) {
+        return null;
+    }
+
+    var routeId = route.id;
+    var options = the[_options];
+
+    return the[_viewMap][routeId] ||
+        (the[_viewMap][routeId] = new View(the[_viewsEl], options.showAnimation, options.hideAnimation));
+};
+
--- src/view.js
+++ src/view.js
@@ -9,3 +9,176 @@
 
 'use strict';
 
+var Class = require('blear.classes.class');
+var modification = require('blear.core.modification');
+var fun = require('blear.utils.function');
+var scopeCSS = require('blear.utils.scope-css');
+var time = require('blear.utils.time');
+
+var isIOS = /iphone|ipad|ipod/i.test(navigator.userAgent);
+var docTitle = doc.title;
+var namespace = 'blearui-application-view';
+var viewId = 0;
+var View = Class.extend({
+    className: 'View',
+    constructor: function (viewsEl, showAnimation, hideAnimation) {
+        var the = this;
+
+        View.parent(the);
+        the.viewsEl = viewsEl;
+        the.el = modification.create('div', {
+            class: namespace,
+            id: namespace + viewId++
+        });
+        the.styleEl = modification.create('style', {
+            class: namespace,
+            id: namespace + viewId++
+        });
+        the[_scrollTop] = 0;
+        the[_showAnimation] = showAnimation;
+        the[_hideAnimation] = hideAnimation;
+        the[_installed] = false;
+    },
+
+    /**
+     * 注入结构
+     * @param html
+     * @param [keepSpace]
+     */
+    html: function (html, keepSpace) {
+        var the = this;
+        the.el.innerHTML = keepSpace ? html : removeHTMLSpace(html);
+        return the;
+    },
+
+    /**
+     * 注入样式
+     * @param text
+     * @param [global] {Boolean} 是否为全局样式
+     */
+    style: function (text, global) {
+        var the = this;
+
+        if (!global) {
+            text = scopeCSS(text, '#' + the.styleEl.id);
+        }
+
+        modification.importStyle(text, the.styleEl, true);
+        return the;
+    },
+
+    /**
+     * 修改文档标题
+     * @param title
+     * @returns {View}
+     */
+    title: function (title) {
+        var the = this;
+
+        // 待修改的标题等于当前标题，则不作为
+        if (title === doc.title) {
+            return the;
+        }
+
+        doc.title = title || docTitle;
+
+        if (!isIOS) {
+            return the;
+        }
+
+        var faviconIframe = modification.create('iframe', {
+            style: {
+                display: 'none'
+            },
+            src: '/favicon.ico'
+        });
+        faviconIframe.onload = function () {
+            time.nextTick(function () {
+                modification.remove(faviconIframe);
+            });
+        };
+        modification.insert(faviconIframe);
+        return the;
+    },
+
+    enter: function (route, ctrl) {
+        var the = this;
+        var options = {
+            direction: route.direction
+        };
+
+        modification.insert(the.styleEl, the.viewsEl);
+        modification.insert(the.el, the.viewsEl);
+
+        if (!the[_installed]) {
+            the[_exec](ctrl.install, route);
+            the[_installed] = true;
+        }
+
+        the[_showAnimation](the.el, options, function () {
+            the[_exec](ctrl.show, route);
+        });
+    },
+
+    leave: function (route, ctrl, callback) {
+        var the = this;
+        var options = {
+            direction: route.direction
+        };
+        the[_hideAnimation](the.el, options, function () {
+            the[_exec](ctrl.hide, route);
+            modification.remove(the.styleEl);
+            modification.remove(the.el);
+            callback();
+        });
+    },
+
+    replace: function (route, ctrl) {
+        this[_exec](ctrl.update, route);
+    }
+});
+var prop = View.prototype;
+var sole = View.sole;
+var _showAnimation = sole();
+var _hideAnimation = sole();
+var _installed = sole();
+var _exec = sole();
+var _scrollTop = sole();
+
+module.exports = View;
+
+prop[_exec] = function (callback, route) {
+    fun.ensure(callback).call(window, this, route);
+};
+
+// ========================================================
+
+/**
+ * 移除 HTML 里的多余空白
+ * @param html
+ * @returns {string}
+ */
+function removeHTMLSpace(html) {
+    var originalRE = /<(textarea|script|pre|code)\b[\s\S]*?>[\s\S]*?<\/\1>/gi;
+    var tagRE = /<\/?[\w-]+[\s\S]*?>/g;
+    var longSpaceRE = /\s+/g;
+    var orginalMap = {};
+    var store = function (source) {
+        var key = '∆' + random.guid() + '∆';
+        orginalMap[key] = source;
+        return key;
+    };
+
+    html = html
+        .replace(originalRE, store)
+        .replace(tagRE, store)
+        .replace(longSpaceRE, '');
+
+    // 恢复
+    object.each(orginalMap, function (key, val) {
+        html = html.replace(key, val);
+    });
+
+    return html;
+}
+
