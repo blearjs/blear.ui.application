900e3a4158d31991b2bdf95c446502249181d226
#云淡然 <cloudcome@163.com>
2018-04-24 11:21:03 +0800

    + 暴露 .view 静态属性

--- src/engine.js
+++ src/engine.js
@@ -35,7 +35,7 @@ var Engine = Class.extend({
             id: id + '-div'
         });
 
-        the[_view] = new View(id, viewsEl, viewEl, styleEl);
+        the.view = new View(id, viewsEl, viewEl, styleEl);
         the[_scrollTop] = 0;
         the[_platform] = platform;
         the[_showAnimation] = showAnimation;
@@ -47,8 +47,9 @@ var Engine = Class.extend({
      * 引擎进入
      * @param route
      * @param ctrl
+     * @param callback
      */
-    enter: function (route, ctrl) {
+    enter: function (route, ctrl, callback) {
         var the = this;
         var options = {
             // 首个 view 进入，则方向为空
@@ -57,7 +58,7 @@ var Engine = Class.extend({
 
         modification.insert(the[_styleEl], the.viewsEl);
         modification.insert(the[_viewEl], the.viewsEl);
-        the[_view].visible = true;
+        the.view.visible = true;
 
         if (!the[_installed]) {
             the[_installed] = true;
@@ -65,7 +66,7 @@ var Engine = Class.extend({
         }
 
         the[_route] = route;
-        the[_view].title(ctrl.title);
+        the.view.title(ctrl.title);
         the[_exec](ctrl.show, route);
         the[_exec](ctrl.update, route);
         the[_showAnimation](the[_viewEl], options, function () {
@@ -74,6 +75,7 @@ var Engine = Class.extend({
             }
 
             layout.scrollTop(the[_viewEl], the[_scrollTop]);
+            callback();
         });
     },
 
@@ -81,14 +83,15 @@ var Engine = Class.extend({
      * 引擎离开
      * @param route
      * @param ctrl
+     * @param callback
      */
-    leave: function (route, ctrl) {
+    leave: function (route, ctrl, callback) {
         // 此时传入的下一个引擎的路由
         var the = this;
         var options = {
             direction: route.direction
         };
-        the[_view].visible = false;
+        the.view.visible = false;
         the[_exec](ctrl.hide, the[_route], route);
         the[_scrollTop] = layout.scrollTop(the[_viewEl]);
         the[_hideAnimation](the[_viewEl], options, function () {
@@ -101,17 +104,18 @@ var Engine = Class.extend({
      * 引擎重启
      * @param route
      * @param ctrl
+     * @param callback
      */
-    reload: function (route, ctrl) {
+    reload: function (route, ctrl, callback) {
         var the = this;
 
         the[_route] = route;
         the[_exec](ctrl.update, route);
+        callback();
     }
 });
 var prop = Engine.prototype;
 var sole = Engine.sole;
-var _view = sole();
 var _showAnimation = sole();
 var _hideAnimation = sole();
 var _installed = sole();
@@ -125,6 +129,6 @@ var _route = sole();
 module.exports = Engine;
 
 prop[_exec] = function (callback, route, nextRoute) {
-    fun.ensure(callback).call(window, this[_view], route, nextRoute);
+    fun.ensure(callback).call(window, this.view, route, nextRoute);
 };
 
